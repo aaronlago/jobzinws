092538c43663d2bd27f1e5f2cac35a0a1c1cacd8
aaronlago <46574305+aaronlago@users.noreply.github.com>
2019-07-11 12:37:42 -0300

    rota
    ordem de rota

--- src/routes/planetaRoute.js
+++ src/routes/planetaRoute.js
@@ -5,10 +5,9 @@ const router = express.Router();
 const controller = require('../controllers/planetaController');
 
 router.post('/create', controller.addPlanet);
+router.get('/list', controller.listPlanets);
 router.get('/find/name/:name', controller.findByName);
 router.get('/find/id/:id', controller.findById);
 router.delete('/delete/:id', controller.delete);
-router.get('/list', controller.listPlanets);
-
 
 module.exports = router;
